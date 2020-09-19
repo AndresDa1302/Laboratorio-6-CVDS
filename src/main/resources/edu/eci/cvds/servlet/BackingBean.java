a339c9d1c43275d8b25ac2d2023a58a9def4a016
Andres Davila <oadavilaf18@gmail.com>
2020-09-19 09:31:56 -0500

    se añadio desde lab 5

---
+++ src/main/resources/edu/eci/cvds/servlet/BackingBean.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.eci.cvds.servlet;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Andres felipe davila
+ * @author Javier Lopez
+ */
+
+
+@ManagedBean(name = "calculadoraBean")
+@SessionScoped
+public class BackingBean implements Serializable{
+    
+    private ArrayList<Double> numberList= new ArrayList<Double>();
+    private double moda;
+    private double varianza;
+    private double desStan;
+    private double media;
+
+    
+    public BackingBean() {
+        
+    }
+
+    public ArrayList<Double> getNumeros() {
+        return numberList;
+    }
+
+    public void setNumeros(ArrayList<Double> numeros) {
+        this.numberList = numeros;
+    }
+
+    public double getModa() {
+        return moda;
+    }
+
+    public void setModa(double moda) {
+        this.moda = moda;
+    }
+
+    public double getVarianza() {
+        return varianza;
+    }
+
+    public void setVarianza(double varianza) {
+        this.varianza = varianza;
+    }
+
+    public double getDesStan() {
+        return desStan;
+    }
+
+    public void setDesStan(double desStan) {
+        this.desStan = desStan;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public void stringToArray(String valores)
+    {
+        String[] valList = valores.split("; ");
+
+        for(String val: valList)
+        {
+            numberList.add(Double.parseDouble(val));
+        }
+    }
+    
+    public void calcularTodo(String valores)
+    {
+        calculateMean(valores);
+        calculateStandardDeviation(valores);
+        calculateVariance(valores);
+        calculateMode(valores);
+    }
+    
+    public double calculateMean(String valores)
+    {
+        stringToArray(valores);
+        media=0;
+        for(int i=0;i<numberList.size();i++)
+        {
+            media+=numberList.get(i);
+        }
+        media=media/numberList.size();
+        
+        return media;
+
+    }
+    public void calculateStandardDeviation(String valores)
+    {
+        desStan=calculateVariance(valores);
+        desStan=Math.sqrt(desStan);
+    }
+    public double calculateVariance(String valores)
+    {
+       double number=calculateMean(valores);
+       varianza=0;
+       for(int i=0;i<numberList.size();i++)
+       {
+           double range;
+           
+           range=Math.pow(numberList.get(i)-number,2);
+           varianza = varianza+range;
+       }
+       varianza= varianza/numberList.size();
+       return varianza;
+    }
+    public void calculateMode(String valores)
+    {
+        stringToArray(valores);
+        moda = numberList.get(0);
+	double estado= numberList.get(0);
+	int maximo = 0;
+	for(int i=0;i<numberList.size();i++)
+	{
+		int cont=0;
+		if(estado==numberList.get(i))
+		{
+			cont++;
+		}else
+		{
+			if(cont>=maximo)
+			{
+				maximo = cont;
+				moda = numberList.get(i);
+			}
+			estado = numberList.get(i);
+		}
+	}
+    }
+    public void restart()
+    {
+        numberList.clear();
+        moda=0;
+        varianza=0;
+        desStan=0;
+        media=0;
+    }
+}
---
+++ target/classes/edu/eci/cvds/servlet/model/Todo.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.servlet.model;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Todo {
+   private int userId=1;
+   private int id=1;
+   private String tittle="delectus aut autem";
+   private boolean completed=false;
+   
+   public Todo(){
+   } 
+   
+   public int getUserId()
+   {
+       return userId;
+   }
+   public int getId()
+   {
+       return id;
+   }
+   public String getTitle()
+   {
+       return tittle;
+   }
+   public boolean getCompleted()
+   {
+       return false;
+   }
+   public void setUserId(int userId)
+   {
+       this.userId=userId;
+   }
+   public void setId (int id)
+   {
+       this.userId=id;
+   }
+   public void setTittle(String tittle)
+   {
+       this.tittle=tittle;
+   }
+   public void setCompleted(boolean completed)
+   {
+       this.completed=completed;
+   }
+}
